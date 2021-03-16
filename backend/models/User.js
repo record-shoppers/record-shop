01d5a2d42be2d9db1ec8c59ad15c294cd73eb52a
Joyce <yourchoice08@gmail.com>
2021-03-16 14:00:28 +0100

    add model and controller for user

---
+++ backend/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const {model, Schema} = mongoose
+
+const UserSchema = new Schema ({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    nickname: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    avatar: {type: String, default: 'http://localhost:5000/statics/avatar.jpg'}
+}, {versionKey: false, timestamps: true})
+
+const User = model('User', UserSchema)
+
+module.exports = User
