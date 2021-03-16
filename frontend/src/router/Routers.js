03b7e797528db9a66f164bfe6abe28f01fbf589a
Elias <elias-guderian@outlook.com>
2021-03-16 15:26:33 +0100

    [add] Routers

---
+++ frontend/src/router/Routers.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Route, Switch, Router } from 'react-router-dom';
+
+const Routers = () => {
+    <Router>
+        <Header />
+        <Switch>
+            <Route path='/' component={ } exact />
+        </Switch>
+        <Footer />
+    </Router>
+}
+
+export default Routers;
