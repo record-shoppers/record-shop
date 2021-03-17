245ed0cfe427f85666a03646e290534e3b17be67
Luke Dowling <lpdowling26@googlemail.com>
2021-03-17 13:57:04 +0100

    [mod] sign up page using react hook forms and styled components

--- frontend/src/router/Routers.js
+++ frontend/src/router/Routers.js
@@ -1,19 +1,21 @@
-import {Route, Switch, } from 'react-router-dom';
-import Dashboard from '../components/Dashboard';
-import Landingpage from '../components/Landingpage';
-import Nav from "../components/Nav"
-import "../css/App.css"
+import { Route, Switch } from "react-router-dom";
+import Dashboard from "../components/Dashboard";
+import Landingpage from "../components/Landingpage";
+import Nav from "../components/Nav";
+import { SignUp } from "../components/SignUp";
+import "../css/App.css";
 
 const Routers = () => {
-    return(
-        <>
-        <Nav/>
-        <Switch>
-            <Route path='/' component={Landingpage} exact />
-            <Route path='/dashboard' component={Dashboard} />
-        </Switch>
-        </>
-    )
-}
+  return (
+    <>
+      <Nav />
+      <Switch>
+        <Route path="/" component={Landingpage} exact />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/signup" component={SignUp} />
+      </Switch>
+    </>
+  );
+};
 
 export default Routers;
