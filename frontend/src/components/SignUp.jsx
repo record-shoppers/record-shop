4f645be4bc06e9dcdd70983bfd467c27715ff5c1
Luke Dowling <lpdowling26@googlemail.com>
2021-03-17 11:13:19 +0100

    [add] signup and private router components

---
+++ frontend/src/components/SignUp.jsx
@@ -0,0 +1,55 @@
+import React, { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+
+export const SignUp = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords don't match");
+    }
+
+    try {
+      setError("");
+      setLoading(true);
+    } catch {
+      setError("Failed to create an account");
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <section>
+      <h2>Sign up</h2>
+      {error && <h2>error}</h2>}
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>Email</label>
+          <input type="email" required ref={emailRef} />
+        </fieldset>
+        <fieldset>
+          <label>Password</label>
+          <input type="password" required ref={passwordRef} />
+        </fieldset>
+        <fieldset>
+          <label>Password Confirm</label>
+          <input type="password" required ref={passwordConfirmRef} />
+        </fieldset>
+        <button disabled={loading} type="submit">
+          Sign up
+        </button>
+      </form>
+      <div>
+        Already have an account? <Link to="/login">Log In</Link>
+      </div>
+    </section>
+  );
+};
