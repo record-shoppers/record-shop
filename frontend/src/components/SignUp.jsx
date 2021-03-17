41b4e25920f3342c65007c499fc4b5e198502302
Luke Dowling <lpdowling26@googlemail.com>
2021-03-17 15:56:33 +0100

    [add] added functionalities to signup page

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
+import randomMan from "../assets/singup-img/randomMan.png";
 
 //styled components
 const Container = styled.main`
@@ -10,12 +11,39 @@ const Container = styled.main`
 const Section = styled.section`
   width: 50%;
   margin: 30px auto;
-  padding: 0 80px;
+  padding: 0 100px;
 `;
 
-const HeadingBold = styled.h3``;
+const HeadingBold = styled.h3`
+  font-size: 28px;
+  line-height: 36px;
+`;
 
-const HeadingNormal = styled.h4``;
+const HeadingNormal = styled.h4`
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  margin-top: 30px;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+`;
+
+const Input = styled.input`
+  margin: 20px 5px;
+  outline: none;
+`;
+
+const Button = styled.button``;
+
+const Image = styled.div`
+  background: url(${randomMan});
+  background-size: 100% 100%;
+  height: 450px;
+  width: 450px;
+`;
 
 // main component
 export const SignUp = () => {
@@ -29,42 +57,44 @@ export const SignUp = () => {
         <HeadingBold>Let us know who you are!</HeadingBold>
         <HeadingNormal>We won't share info with anybody</HeadingNormal>
         <HeadingNormal>I promise</HeadingNormal>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input
-            name="firstName"
-            ref={register({ required: true, maxLength: 25 })}
-            placeholder="First Name"
-          />
-          <input
-            name="lastName"
-            ref={register({ required: true, maxLength: 25 })}
-            placeholder="Last Name"
-          />
-          <input
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <InputContainer>
+            <Input
+              name="firstName"
+              ref={register({ required: true, maxLength: 25 })}
+              placeholder="First Name"
+            />
+            <Input
+              name="lastName"
+              ref={register({ required: true, maxLength: 25 })}
+              placeholder="Last Name"
+            />
+          </InputContainer>
+          <Input
             name="email"
             ref={register({ required: true })}
             placeholder="Last Name"
           />
-          <input
+          <Input
             name="nickName"
             ref={register({ required: true })}
             placeholder="Nickname"
           />
-          <input
+          <Input
             name="password"
             ref={register({ required: true })}
             placeholder="Password"
           />
-          <input
+          <Input
             name="confirmPassword"
             ref={register({ required: true })}
             placeholder="Repeat password"
           />
-          <input type="submit" />
-        </form>
+          <Button type="submit">Create account</Button>
+        </Form>
       </Section>
       <Section>
-        <h2>I am an image</h2>
+        <Image />
       </Section>
     </Container>
   );
