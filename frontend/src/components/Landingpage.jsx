32780def70840418042e59867a41c01aedecdba7
INes <inesfcnogueira@gmail.com>
2021-03-18 10:47:06 +0100

    before changing branch

--- frontend/src/components/Landingpage.jsx
+++ frontend/src/components/Landingpage.jsx
@@ -1,12 +1,30 @@
-import React from "react";
-import homepageImg from "../assets/homepageImg.png"
+import recordshop from '../assets/recordshop.jpg';
+import styled from 'styled-components'
 
-const Homepage = ()=>{
-    return(
-        <div style={{ backgroundImage: {homepageImg} }}>
-            <h1>Homepage</h1>
-        </div>
+const Landingpage = () => {
+
+    const Background = styled.div`
+    background: url(${recordshop});
+    background-size: 100% 100%;
+    height:90%;`;
+
+    const Button = styled.a`
+    border-radius: 2.3rem;
+    padding: 10px;
+    background-color: #fff;
+    position:relative;
+    top: 95%;
+    left:45%;
+    font-weight:bold;
+    font-size:10px;`;
+    
+    return (
+        <>
+            <Background>
+                <Button>Illustrations by Gabriel Hollington</Button>
+            </Background>
+        </>
     )
 }
 
-export default Homepage;
+export default Landingpage;
