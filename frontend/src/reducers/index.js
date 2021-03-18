b23688865f0e32d2de6de116590022f61970a85b
Elias <elias-guderian@outlook.com>
2021-03-18 14:26:10 +0100

    [ADD] userReducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
 import { recordReducer } from './recordReducer';
+import { userReducer } from './userReducer';
 
-export default combineReducers({recordReducer});
+export default combineReducers({recordReducer, userReducer});
