b2cc4c85cb7b48bc3ac48d27c93ff68908d44e04
Luke Dowling <lpdowling26@googlemail.com>
2021-03-22 14:51:45 +0100

    [mod] updated backend and front end user profile

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 import Johnlenon from "../assets/John+Lennon.jpg";
 import Ghost from "../assets/Ghost.JPG";
@@ -27,22 +28,18 @@ export const UserProfile = () => {
     @media (max-width: 768px) {
       margin-bottom: 30px;
     }
-
   `;
 
   const RightSection = styled.div`
     background-color: #f1efff;
 
     width: 100%;
-
   `;
 
   const SectionContainer = styled.div`
     margin: 10% auto;
 
-
     width: 75%;
-
   `;
 
   const Parag = styled.p`
@@ -67,29 +64,13 @@ export const UserProfile = () => {
     display: flex;
     img {
       margin: 10px;
-    }
-  `;
-
-  const Button = styled.button`
-    float: right;
-    font-size: 20px;
-  `;
-
-  const ImgContainer = styled.div`
-    margin-top: 10%;
-    display: flex;
-    img {
-      margin: 10px;
       border-radius: 50%;
       border: 2px solid #eea668;
     }
-     @media (max-width: 920px) {
+    @media (max-width: 920px) {
       flex-direction: column;
     }
   `;
-
-
-   
 
   const SelectedPic = styled.div`
     img {
@@ -99,7 +80,7 @@ export const UserProfile = () => {
   `;
 
   const Thumbnails = styled.div`
- display: grid;
+    display: grid;
     grid-template-columns: repeat(3, 1fr);
     gap: 10px;
     img {
@@ -108,43 +89,79 @@ export const UserProfile = () => {
     }
   `;
 
-
+  const history = useHistory();
   const dispatch = useDispatch();
   const picture = useSelector((state) => state.profileReducer);
   const user = useSelector((state) => state.loginReducer);
-    const [selectedPic, setSelectedPic] = useState('');
+  const [selectedPic, setSelectedPic] = useState("");
+  const [userInformation, setUserInformation] = useState({
+    firstName: "",
+    lastName: "",
+    email: user.user.data.email,
+    password: user.user.data.password,
+  });
 
-  const handleClick = async (e) => {
-    console.log("this is the user =>", user);
-    const pic = e.target.src;
-    let isUpdated = await updateAvatar(user._id, pic);
-    console.log(isUpdated);
-    dispatch(saveProfile(pic));
-     setSelectedPic(e.target.name);
+  const changeHanler = (e) => {
+    setUserInformation({ ...userInformation, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
-    console.log(e);
+  const handleClick = async (e) => {
+    const pic = e.target.src;
+    let updatedAvatar = await updateAvatar(user.user.data._id, pic);
+    dispatch(saveProfile(pic));
+    setSelectedPic(e.target.name);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (userInformation.firstName && userInformation.lastName) {
+      let updatedInformation = await updateInformation(
+        user.user.data._id,
+        userInformation
+      );
+      history.push("/dashboard");
+    }
   };
 
   return (
     <Main>
       <LeftSection>
         <SectionContainer>
-          <h1>Your Profile, Mr.Wasabis</h1>
+          <h1>
+            Your Profile, {user.user.data.firstName} {user.user.data.lastName}
+          </h1>
           <Parag>
             Don't Forget to click the save button before you are gone
           </Parag>
 
           <form onSubmit={handleSubmit}>
             <NameInput>
-              <input type="text" name="firstName" placeholder="Steve" />
-              <input type="text" name="lastName" placeholder="Steveson" />
+              <input
+                type="text"
+                name="firstName"
+                placeholder={user.user.data.firstName}
+                onChange={changeHanler}
+                value={userInformation.firstName}
+              />
+              <input
+                type="text"
+                name="lastName"
+                placeholder={user.user.data.lastName}
+                onChange={changeHanler}
+                value={userInformation.lastName}
+              />
             </NameInput>
-            <input type="email" name="email" placeholder="me@gmail.com" />
-            <input type="password" name="password" placeholder="01234" />
+            <input
+              type="email"
+              name="email"
+              placeholder={user.user.data.email}
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder={user.user.data.password}
+            />
             <Button type="submit">Save</Button>
-
           </form>
         </SectionContainer>
       </LeftSection>
@@ -156,103 +173,85 @@ export const UserProfile = () => {
             <SelectedPic>
               <img
                 src={picture}
-
-                alt='ghost ilustration'
-                className='selected-pic'
-
+                alt="ghost ilustration"
+                className="selected-pic"
               />
             </SelectedPic>
             <Thumbnails>
               <img
                 src={Ghost}
-
-                alt='ghost ilustration'
-                name='Ghost'
-                className={selectedPic === 'Ghost' ? 'active' : 'selected-pic'}
+                alt="ghost ilustration"
+                name="Ghost"
+                className={selectedPic === "Ghost" ? "active" : "selected-pic"}
                 onClick={handleClick}
               />
               <img
                 src={Johnlenon}
-
-                alt='John Lennon illustration'
-                name='Johnlenon'
+                alt="John Lennon illustration"
+                name="Johnlenon"
                 className={
-                  selectedPic === 'Johnlenon' ? 'active' : 'selected-pic'
+                  selectedPic === "Johnlenon" ? "active" : "selected-pic"
                 }
-
                 onClick={handleClick}
               />
               <img
                 src={Melbourne}
-
-
-                alt='Melbourne illustration'
-                name='Melbourne'
+                alt="Melbourne illustration"
+                name="Melbourne"
                 className={
-                  selectedPic === 'Melbourne' ? 'active' : 'selected-pic'
+                  selectedPic === "Melbourne" ? "active" : "selected-pic"
                 }
-
                 onClick={handleClick}
               />
               <img
                 src={Record}
-
-                alt='Record illustration'
-                name='Record'
-                className={selectedPic === 'Record' ? 'active' : 'selected-pic'}
-
+                alt="Record illustration"
+                name="Record"
+                className={selectedPic === "Record" ? "active" : "selected-pic"}
                 onClick={handleClick}
               />
               <img
                 src={Watchout}
-                alt='Watchout illustration'
-                name='Watchout'
+                alt="Watchout illustration"
+                name="Watchout"
                 className={
-                  selectedPic === 'Watchout' ? 'active' : 'selected-pic'
+                  selectedPic === "Watchout" ? "active" : "selected-pic"
                 }
-
                 onClick={handleClick}
               />
               <img
                 src={WeirdPig}
-
-                alt='Weird Pig illustration'
-                name='WeirdPig'
+                alt="Weird Pig illustration"
+                name="WeirdPig"
                 className={
-                  selectedPic === 'WeirdPig' ? 'active' : 'selected-pic'
+                  selectedPic === "WeirdPig" ? "active" : "selected-pic"
                 }
-
                 onClick={handleClick}
               />
               <img
                 src={WeirdPriestress}
-
-                alt='Weird Priestress illustration'
-                name='WeirdPriestress'
+                alt="Weird Priestress illustration"
+                name="WeirdPriestress"
                 className={
-                  selectedPic === 'WeirdPriestress' ? 'active' : 'selected-pic'
+                  selectedPic === "WeirdPriestress" ? "active" : "selected-pic"
                 }
-
                 onClick={handleClick}
               />
               <img
                 src={Weirddog}
-
-                alt='Weird dog illustration'
-                name='Weirddog'
+                alt="Weird dog illustration"
+                name="Weirddog"
                 className={
-                  selectedPic === 'Weirddog' ? 'active' : 'selected-pic'
+                  selectedPic === "Weirddog" ? "active" : "selected-pic"
                 }
-
                 onClick={handleClick}
               />
               <img
                 src={Wathever}
-
-                alt='Whatever illustration'
-                name='Wathever'
+                alt="Whatever illustration"
+                name="Wathever"
                 className={
-                  selectedPic === 'Wathever' ? 'active' : 'selected-pic'
+                  selectedPic === "Wathever" ? "active" : "selected-pic"
                 }
                 onClick={handleClick}
               />
